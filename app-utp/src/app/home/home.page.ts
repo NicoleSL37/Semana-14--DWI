d49d178529a4402175a1bc8dcc5b4fd8c09ed62d
Nicole Sobrino <sobrinoluza@gmail.com>
2026-06-25 13:29:31 -0500

    Semana 11 app base Ionic Angular con formulario

--- app-utp/src/app/home/home.page.ts
+++ app-utp/src/app/home/home.page.ts
@@ -9,29 +9,22 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class HomePage {
   enviado = false;
-
   registroForm = this.fb.group({
     nombre: ['', [Validators.required, Validators.minLength(3)]],
     correo: ['', [Validators.required, Validators.email]],
     tipoApp: ['', Validators.required],
   });
-
   constructor(private fb: FormBuilder) {}
-  get f() { return this.registroForm.controls; }
-
-  validarRegistro() {
-    this.enviado = true; // Activa la visualización de errores
-
-    if (this.registroForm.invalid) {
-      console.log('Formulario inválido');
-      return; // Detiene la ejecución si hay errores
-    }
-
-    // Si todo está bien:
-    console.log('Formulario enviado:', this.registroForm.value);
-    alert('Registro exitoso: ' + this.registroForm.value.nombre);
-    
-    // Aquí después añadirás la navegación a la página de detalle
+  get f() {
+    return this.registroForm.controls;
   }
 
+  validarRegistro() {
+    this.enviado = true;
+    if (this.registroForm.invalid) {
+      this.registroForm.markAllAsTouched();
+      return;
+    }
+    console.log('Registro válido', this.registroForm.value);
+  }
 }
